8e6aaef9af6cb6fdbdb2e1d75b4d7b19595e2016
Envy <envy1396@gmail.com>
2023-09-05 18:09:10 +0300

    not a lot, just experiments with arch

---
+++ CinameToGether/MessageHandler.cs
@@ -0,0 +1,22 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace CinameToGether
+{
+    public class MessageHandler
+    {
+        private readonly ITelegramBotClient _client;
+
+        public MessageHandler(ITelegramBotClient client)
+        {
+            _client = client;
+        }
+
+        public async Task Process(Message message)
+        {
+            var username = message?.From?.Username;
+            if (message != null)
+                await _client.SendTextMessageAsync(message.Chat.Id, $"hello {username}");
+        }
+    }
+}
--- CinameToGether/Program.cs
+++ CinameToGether/Program.cs
@@ -1,18 +1,63 @@
-﻿using System.Text.Json;
+﻿using CinameToGether;
 using Telegram.Bot;
-using CinameToGether.Dal.Context;
-using CinameToGether.Dal.Models;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 var authToken = Environment.GetEnvironmentVariable("ACCESS_TOKEN");
+
 if (String.IsNullOrEmpty(authToken))
 {
     return;
 }
 
-var db = new ApplicationDbContext();
-
-Console.WriteLine(JsonSerializer.Serialize(db.Users));
-
 var botClient = new TelegramBotClient(authToken);
 
-Console.Read();
+using CancellationTokenSource cts = new ();
+
+ReceiverOptions receiverOptions = new ()
+{
+    AllowedUpdates = Array.Empty<UpdateType>()
+};
+
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    if (update.Message is not { } message)
+        return;
+    if (message.Text is not { } messageText)
+        return;
+
+    var chatId = message.Chat.Id;
+
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+    var handler = new MessageHandler(botClient);
+    await handler.Process(message);
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
